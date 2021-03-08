42b1538ffa0be6b2c3013f6bb71452518a441dd5
wj.cao <595842570@qq.com>
2021-03-08 10:43:06 +0800

    商品列表布局

--- src/components/buyCat/index.jsx
+++ src/components/buyCat/index.jsx
@@ -25,10 +25,11 @@ class BuyCat extends Component {
   render() {
     let { food } = this.props;
     let { foodNum } = this.state;
+    let flag = foodNum === 0 ? false : true;
     const single = (
       <div className="cart_button">
         <CSSTransition
-          in={foodNum}
+          in={flag}
           timeout={300}
           classNames="leftIcon"
           unmountOnExit
@@ -45,9 +46,35 @@ class BuyCat extends Component {
         ></div>
       </div>
     );
+    const multiple = (
+      <div className="cart_button">
+        <CSSTransition
+          in={flag}
+          timeout={300}
+          classNames="leftIcon"
+          unmountOnExit
+        >
+          <div
+            className="iconfont icon-jian"
+            onClick={this.handlerSubtract.bind(this)}
+          ></div>
+        </CSSTransition>
+        <span className="cart_num">{foodNum ? foodNum : ""}</span>
+        {flag ? (
+          <div
+            className="iconfont icon-jia"
+            onClick={this.handlerAdd.bind(this)}
+          ></div>
+        ) : (
+          <div className="show_chooselist" onClick={this.handlerAdd.bind(this)}>
+            选规格
+          </div>
+        )}
+      </div>
+    );
     return (
       <div className="cart_module">
-        {food.specifications.length ? "" : single}
+        {food.specifications.length ? multiple : single}
       </div>
     );
   }
